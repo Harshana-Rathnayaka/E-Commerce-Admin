ef5380f26d8921cbb8f484157d573f0dbaa116a7
Harshana ratnayaka <huratnayaka@gmail.com>
2020-10-17 16:43:02 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:admin_app/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Dashboard(),
+    );
+  }
+}
